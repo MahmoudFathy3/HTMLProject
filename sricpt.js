cc78b85882c75297ec817f97a298110a0cf2e172
MahmoudFathyCode <88001901+MahmoudFathyCode@users.noreply.github.com>
2021-07-26 21:26:31 +0200

    Add files via upload

---
+++ sricpt.js
@@ -0,0 +1,11 @@
+
+document.getElementById("b1").onclick = function(){
+    alert("Welcome in The Project");
+}
+
+document.getElementById("confirm").onclick = function(){
+    document.getElementById("name").innerHTML ="Welcome Mahmoud in JavaScript";
+    document.getElementById("confirm").style.display= "none";
+    document.getElementById("nam").style.display= "none";
+
+}
